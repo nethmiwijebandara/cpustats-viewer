145bf3adf4910c6845d3a413104fd011b8a6282a
nethmiwijebandara <54525055+nethmiwijebandara@users.noreply.github.com>
2021-02-12 23:10:05 +0530

    initial commit

---
+++ frontend/src/components/CPUUsage.vue
@@ -0,0 +1,55 @@
+<template>
+  <apexchart type="radialBar" :options="options" :series="series"></apexchart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      series: [0],
+      options: {
+        labels: ['CPU Usage']
+      }
+    };
+  },
+  mounted: function() {
+    // eslint-disable-next-line no-undef
+    wails.Events.On("usage", cpu_usage => {
+      if (cpu_usage) {
+        this.series = [ cpu_usage.avg ];
+      }
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin-top: 2em;
+  position: relative;
+  min-height: 5rem;
+  width: 100%;
+}
+a:hover {
+  font-size: 1.7em;
+  border-color: blue;
+  background-color: blue;
+  color: white;
+  border: 3px solid white;
+  border-radius: 10px;
+  padding: 9px;
+  cursor: pointer;
+  transition: 500ms;
+}
+a {
+  font-size: 1.7em;
+  border-color: white;
+  background-color: #121212;
+  color: white;
+  border: 3px solid white;
+  border-radius: 10px;
+  padding: 9px;
+  cursor: pointer;
+}
+</style>
---
+++ go.mod
@@ -0,0 +1,11 @@
+module cd-cpustats
+
+go 1.15
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/leaanthony/mewn v0.10.7
+	github.com/shirou/gopsutil v3.20.12+incompatible
+	github.com/wailsapp/wails v1.11.0
+)
